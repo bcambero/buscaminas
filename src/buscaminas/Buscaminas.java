ea70302828241882c1c6116ebe40d3297985f4a2
Borja <Borja@192.168.1.128>
2013-03-21 20:54:41 +0100

    errores por todos lados

--- src/buscaminas/Buscaminas.java
+++ src/buscaminas/Buscaminas.java
@@ -3,23 +3,46 @@ import java.util.Scanner;
 public class Buscaminas {
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner sc= new Scanner (System.in);
+        tablero();
+}
+    public static void tablero(){
         System.out.println("Bienvenidos al buscaminas");
+        Scanner sc= new Scanner (System.in);
         System.out.println("Introduzca la fila");
         int filaIntroducida=sc.nextInt();
         System.out.println("Introduzca la columna");
         int columnaIntroducida=sc.nextInt();
-        tablero[filaIntroducida][columnaIntroducida];
-}
-    public static void tablero(){
         boolean fin=false;
         String [][]tablero={{"mina","2","1","1","0"},
                             {"1","2","mina","2","0"},
                             {"0","2","mina","2","0"},
                             {"0","1","1","2","1"},
                             {"0","0","1","mina","1"}};
-        for (int filas=0;filas<tablero.length;filas++){
-            for (int columnas=0;columnas<tablero.length;columnas++){
+
+        while(fin==false){
+            while(fin==false){
+                if (tablero[filaIntroducida][columnaIntroducida]=="1"){
+                    System.out.println("Tienes 1 mina alrededor");
+                    }
+                if (tablero[filaIntroducida][columnaIntroducida]=="2"){
+                    System.out.println("Tienes 2 minas alrededor");
+                    }
+                if (tablero[filaIntroducida][columnaIntroducida]=="0"){
+                    System.out.println("No hay nada ahí");
+                    }
+                if (tablero[filaIntroducida][columnaIntroducida]=="mina")
+                    {
+                    System.out.println("Has descubierto una mina, fin del juego.");
+                    fin=true;
+                    }
+            }
+            System.out.println("Introduzca otra fila");
+            filaIntroducida=sc.nextInt();
+            System.out.println("Introduzca otra columna");
+            columnaIntroducida=sc.nextInt();
+        }
+        /*for (int filas=0;(filas<tablero.length)&&(fin==false);filas++){
+            for (int columnas=0;(columnas<tablero.length)&&(fin==false);columnas++){
                 if((tablero[filas][columnas])=="mina"){
                     System.out.println("Has fallado. Fin del juego");
                     fin=true;
@@ -30,11 +53,9 @@ public class Buscaminas {
                 if(tablero[filas][columnas]=="2"){
                     System.out.println("2 minas alredor, introduzca otra casilla");
                 }
-                if (tablero[filas][columnas]=="1"){
+                if (tablero[filas][columnas]=="0"){
                     System.out.println("No hay nada ahí");
                 }                
-            }
+            }*/
         }
     }
-}
-
