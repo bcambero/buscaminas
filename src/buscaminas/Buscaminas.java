4c5b4feb9e45cda759dc051a66990df9e93aea7e
Borja <Borja@192.168.1.128>
2013-03-21 18:58:45 +0100

    comienzo del buscaminas

--- src/buscaminas/Buscaminas.java
+++ src/buscaminas/Buscaminas.java
@@ -1,6 +1,8 @@
 package buscaminas;
+import java.util.Scanner;
 public class Buscaminas {
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc= new Scanner (System.in);
     }
 }
