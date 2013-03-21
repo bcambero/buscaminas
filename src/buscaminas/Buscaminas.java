1b5111c8002f6f737cee7285ab8ca1bcc27320a0
Borja <Borja@192.168.1.128>
2013-03-21 19:47:51 +0100

    implementando un tablero con los valores y las minas ya asignados

--- src/buscaminas/Buscaminas.java
+++ src/buscaminas/Buscaminas.java
@@ -4,5 +4,17 @@ public class Buscaminas {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc= new Scanner (System.in);
+        
+        
+}
+    public static void tablero(){
+        String [][]tablero={{"mina","2","1","1","0"},
+                            {"1","2","mina","2","0"},
+                            {"0","2","mina","2","0"},
+                            {"0","1","1","2","1"},
+                            {"0","0","1","mina","1"}};
+        
+        
     }
 }
+
