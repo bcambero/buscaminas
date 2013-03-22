4d51b1e804936afe0e2857bf04a9e9fad1e7bd53
bcambero <borjacamb1988@gmail.com>
2013-03-22 09:36:50 +0100

    Update Buscaminas.java

--- src/buscaminas/Buscaminas.java
+++ src/buscaminas/Buscaminas.java
@@ -8,19 +8,17 @@ public class Buscaminas {
     public static void tablero(){
         System.out.println("Bienvenidos al buscaminas");
         Scanner sc= new Scanner (System.in);
-        System.out.println("Introduzca la fila");
-        int filaIntroducida=sc.nextInt();
-        System.out.println("Introduzca la columna");
-        int columnaIntroducida=sc.nextInt();
         boolean fin=false;
         String [][]tablero={{"mina","2","1","1","0"},
                             {"1","2","mina","2","0"},
                             {"0","2","mina","2","0"},
                             {"0","1","1","2","1"},
                             {"0","0","1","mina","1"}};
-
         while(fin==false){
-            while(fin==false){
+                System.out.println("Introduzca la fila");
+                int filaIntroducida=sc.nextInt();
+                System.out.println("Introduzca la columna");
+                int columnaIntroducida=sc.nextInt();            
                 if (tablero[filaIntroducida][columnaIntroducida]=="1"){
                     System.out.println("Tienes 1 mina alrededor");
                     }
@@ -30,16 +28,12 @@ public class Buscaminas {
                 if (tablero[filaIntroducida][columnaIntroducida]=="0"){
                     System.out.println("No hay nada ahí");
                     }
-                if (tablero[filaIntroducida][columnaIntroducida]=="mina")
-                    {
+                if (tablero[filaIntroducida][columnaIntroducida]=="mina"){
                     System.out.println("Has descubierto una mina, fin del juego.");
                     fin=true;
                     }
-            }
-            System.out.println("Introduzca otra fila");
-            filaIntroducida=sc.nextInt();
-            System.out.println("Introduzca otra columna");
-            columnaIntroducida=sc.nextInt();
+            
+
         }
         /*for (int filas=0;(filas<tablero.length)&&(fin==false);filas++){
             for (int columnas=0;(columnas<tablero.length)&&(fin==false);columnas++){
